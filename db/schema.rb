344ec881d4ab7b4ce9d40c32c7a54f4c266baa8c
laury bretteville <laury.bretteville@edhec.com>
2018-10-17 11:53:51 +0200

    models all green

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_092120) do
+ActiveRecord::Schema.define(version: 2018_10_17_095131) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_17_092120) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
   end
 
 end
